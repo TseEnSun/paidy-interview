595793d91e9818f7b114595c5dac1c59c95415e0
Sean Sun <tseensun@gmail.com>
2022-12-21 17:17:24 +0800

    Update syntax

--- forex-mtl/src/main/scala/forex/http/health/HealthRoute.scala
+++ forex-mtl/src/main/scala/forex/http/health/HealthRoute.scala
@@ -1,11 +1,11 @@
-package forex.http.health
+package forex.http
+package health
 
 import cats.effect.Sync
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 import forex.services.HealthCheckService
-import forex.http.jsonEncoder
 
 
 class HealthRoute[F[_]: Sync](healthCheck: HealthCheckService[F]) extends Http4sDsl[F]{
